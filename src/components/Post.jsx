76fb0e3c9c961c8766d2d742e8df3c3fb50d1900
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-07-27 00:33:21 +0530

    Add Social Media app components and UI styling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,22 @@
-const Post = () => {
+import { AiFillDelete } from "react-icons/ai";
+
+const Post = ({ post }) => {
   return (
-    <div class="card" style={{ width: "18rem" }}>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+    <div className="card post-card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title">
+          {post.title}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            <AiFillDelete />
+          </span>
+        </h5>
+        <p className="card-text">{post.body}</p>
+        {post.tags.map((tag) => (
+          <span class="badge text-bg-primary hashtag">{tag}</span>
+        ))}
+        <div class="alert alert-success reaction" role="alert">
+          A simple success alert—check it out!
+        </div>
       </div>
     </div>
   );
---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,49 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST,
+  );
+
+  const addPost = () => {};
+
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations.Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Paas ho bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain pass. hard to believe.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
