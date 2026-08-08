2479a7d1081eadbe0c3d6fa6f29d74a3408fe411
pathareshubham98-dot <pathareshubham98@gmail.com>
2026-08-09 00:07:51 +0530

    add home and create post navigation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
@@ -9,20 +10,28 @@ const PostList = () => {
 
   useEffect(() => {
     setFetching(true);
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
         setFetching(false);
       });
+
+    return () => {
+      console.log("Cleaning up UseEffect.");
+      controller.abort();
+    };
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
